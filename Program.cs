4cce4d5d41e920e70ce1e0d8bf826e1162d78f70
ayush kishor <ayushkishor3@gmail.com>
2022-11-02 15:00:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Employee{  //Base Class containing an employee details
+    public String FirstName;
+    public String LastName;
+    public int salary;
+
+    public void printname()
+    {
+        Console.WriteLine(FirstName + " "+LastName);
+    }
+}
+public class fulltimeemployee:Employee //inheriting base class
+{
+   public float netsal = 500000;
+}
+public class Intern : Employee
+{
+   public float hourlysal = 100; // Inheriting
+}
+
+public class inheritance
+{
+    public static void Main()
+    {
+        fulltimeemployee employee = new fulltimeemployee();
+        employee.FirstName = "harry";
+        employee.LastName = "moris";
+        employee.salary = 7000000;
+        employee.printname();
+
+        Intern i=new Intern();
+        i.FirstName = "code";
+        i.LastName = "buster";
+        i.hourlysal = 100;
+        i.printname();  
+
+    }
+}
